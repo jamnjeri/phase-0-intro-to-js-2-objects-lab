22b0b1311bf37c9ac3ba64cba4fe3d9dac6fcd99
Jamila <jamnjeri@gmail.com>
2022-11-21 10:41:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+// A program to keep track of a company's employees and their data
+const employee = {
+    name: "Jay",
+    streetAddress: "34 Nairobi",
+};
+//New object with updated value for the key passed in
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee}
+  
+    newEmployee[key] = value;
+  
+    return newEmployee;
+}
+  //Destructively Update Employee with Key and Value
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]= value;
+
+    return employee;
+}
+//Non-destructvely delete from the employee object
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newEmployee = { ...employee };
+  
+    delete newEmployee[key];
+  
+    return newEmployee;
+  }
+  //Destructvely delete from the employee object
+  function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key];
+  
+    return employee;
+  }
